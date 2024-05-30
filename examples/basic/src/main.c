17f214a5789530f89aef7a8ca2f4451ea51a9b1e
Conor Golden <conorgolden1@hotmail.com>
2024-05-30 10:35:51 -0400

    removed bloat

--- examples/basic/src/main.c
+++ examples/basic/src/main.c
@@ -1,11 +1,8 @@
 #include "../testify/test_assert.h"
-#include <stdio.h>
 
-int main() {
-    printf("DO NOT PRINT\n");
-}
+int main() {}
 
 void testFunc() {
-    assert(1 == 1);
-    assert(1 == 2);
+  assert(1 == 1);
+  assert(1 == 2);
 }
